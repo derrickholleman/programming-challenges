9df9de793a61bf078af1bc0506fbd9a9b3b3c1a3
Derrick Holleman <derrickholleman@gmail.com>
2021-12-04 17:11:18 -0600

    palindrome checker allows for numbers with commas

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,20 @@
 // check if a number is a palindrome
 
+
+
 function isPalindrome(input) {
   // if string, get rid of potential spaces
   if (typeof input === "string") {
-    input = input.replace(/\s/g, '')
+    input = input.replace(/[^\w]/g, '')
+  }
+  // if number, change to string and remove any commas
+  if (typeof input === "number") {
+    input = input.toString().replace(/,/g, '')
   }
 
   if (
-    input.toString().split("").reverse().join("") ===
-    input.toString().split().join("")
+    input.toString().split("").reverse().join("").toLowerCase() ===
+    input.toString().split().join("").toLowerCase()
   ) {
     return true;
   } else {
@@ -16,4 +22,4 @@ function isPalindrome(input) {
   }
 }
 
-console.log(isPalindrome("nurses run"));
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
